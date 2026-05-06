c660cd9b4b31d8af639b1a108d11db988efb494a
MelesAnneaud <2020Visiongkt@gmail.com>
2026-05-06 18:30:52 -0400

    Update app.py

--- app.py
+++ app.py
@@ -36,3 +36,50 @@ def get_resume():
         media_type="application/pdf",
         filename=file_path.name
     )
+
+from fastapi import FastAPI
+
+app = FastAPI()
+
+users = [
+    {"id": 1, "name": "Meles", "role": "Junior Backend Developer"},
+    {"id": 2, "name": "John", "role": "IT Support"}
+]
+
+@app.get("/")
+def home():
+    return {"message": "Backend API is running"}
+
+@app.get("/users")
+def get_users():
+    return users
+
+@app.get("/users/{user_id}")
+def get_user(user_id: int):
+    for user in users:
+        if user["id"] == user_id:
+            return user
+    return {"error": "User not found"}
+
+@app.post("/users")
+def create_user(id: int, name: str, role: str):
+    new_user = {"id": id, "name": name, "role": role}
+    users.append(new_user)
+    return {"message": "User created", "user": new_user}
+
+@app.put("/users/{user_id}")
+def update_user(user_id: int, name: str, role: str):
+    for user in users:
+        if user["id"] == user_id:
+            user["name"] = name
+            user["role"] = role
+            return {"message": "User updated", "user": user}
+    return {"error": "User not found"}
+
+@app.delete("/users/{user_id}")
+def delete_user(user_id: int):
+    for user in users:
+        if user["id"] == user_id:
+            users.remove(user)
+            return {"message": "User deleted"}
+    return {"error": "User not found"}
